a289779b7b33594dad1e219a84ab5215335e2a58
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-12 11:46:15 +0200

    [REFACTOR] Remove focus trap with inertSelector

--- packages/typo3_vite_demo/Resources/Private/JavaScript/main.js
+++ packages/typo3_vite_demo/Resources/Private/JavaScript/main.js
@@ -8,27 +8,23 @@ preload();
 new ExpandingNav({
   rootElement: document.querySelector(".mainnav"),
   buttonSelector: ".mainnav__button",
-  inertSelector: "main",
 });
 
 new ExpandingNav({
   rootElement: document.querySelector(".subnav"),
   buttonSelector: ".subnav__button, .subnav__close",
   closeButtonSelector: ".subnav__close",
-  inertSelector: "main, .logo, .header__metanav, .mainnav__button",
   hover: true
 });
 
 new ExpandingNav({
   rootElement: document.querySelector(".header__metanav--desktop > .search"),
   buttonSelector: ".search__button",
-  inertSelector: "main"
 });
 
 new ExpandingNav({
   rootElement: document.querySelector(".header__metanav--mobile > .search"),
   buttonSelector: ".search__button",
-  inertSelector: "main"
 });
 
 
